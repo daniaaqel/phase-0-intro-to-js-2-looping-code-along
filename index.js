b7c3958f1b07849b603837f31f50f53d1c1dbdc1
daniaaqel <dania.aqel@hotmail.com>
2022-09-07 11:18:17 +0300

    Commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,something){
+    const newarray=[];
+for(let i =0 ; i < names.length;i++){
+    var string = `Thank you, ${names[i]}, for the wonderful ${something} gift!`;
+    newarray.push(string);
+}
+return newarray;
+}
+
+function countDown(number){
+for(let i=number; i >=0;i--)
+console.log(i);
+}
+
